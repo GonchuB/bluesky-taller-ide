2775fff0acee450e4750ecd268b521a9ab4722b9
Gonzalo Beviglia <gonzalo.beviglia@fdvsolutions.com>
2014-03-26 21:46:36 -0300

    Metodo paralelo al main para ejecutar sin UI.

--- IDE/src/Main.java
+++ IDE/src/Main.java
@@ -1,25 +1,44 @@
-import main.apis.HEXAConversionAPI;
-import main.ui.Editor;
-import main.apis.bitvector.BitVector;
+import main.model.Compilador;
 import main.model.Simulador;
+import main.ui.Editor;
 
 /**
  * Created by Juan-Asus on 20/03/2014.
  */
 public class Main {
     public static void main(String[] args) {
-        /*Simulador simulador = new Simulador();
-        simulador.init("D:\\Workspace\\bluesky-taller-ide\\IDE\\src\\resources\\programas\\ejemplo.maq");
-        simulador.iniciarSimulacionCompleta();
-        simulador.mostrarEstadoSimulacion();
 
-        System.exit(0);*/
+//        runWhitoutUI();
+
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
-        	 
             @Override
             public void run() {
                 new Editor().getJFrame().setVisible(true);    //hace visible la GUI creada por la clase TPEditor
             }
         });
     }
+
+    public static void runWhitoutUI() {
+
+        /**
+         * CAMBIAR ARCHIVO ESPECIFICO DE CADA MAQUINA.
+         */
+        String rutaArchivoASM = "/home/gonchub/Documents/FIUBA/bluesky-taller-ide/IDE/src/resources/programas/test.asm";
+
+        Compilador compilador = new Compilador();
+        String rutaArchivoMAQ = rutaArchivoASM.replace(".asm", ".maq");
+        String status = compilador.compilar(rutaArchivoASM);
+
+        if (status != null) {
+            System.out.println(status);
+        } else {
+            System.out.println("Compilación exitosa - Se creó el archivo " + rutaArchivoMAQ);
+        }
+
+        Simulador simulador = new Simulador();
+        simulador.init(rutaArchivoMAQ);
+        simulador.iniciarSimulacionCompleta();
+        simulador.mostrarEstadoSimulacion();
+    }
+
 }
