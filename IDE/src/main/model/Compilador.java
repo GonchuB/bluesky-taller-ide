7eafc2cbacc59aaf7c0c54e35fc34118d4c0bf8a
juan.marchese <juan.marchese@fdvsolutions.com>
2014-03-31 15:51:15 -0300

    saco todo

--- IDE/src/main/model/Compilador.java
+++ IDE/src/main/model/Compilador.java
@@ -11,8 +11,6 @@ import java.util.Map;
 /**
  * Created by Juan-Asus on 21/03/2014.
  */
-//FIXME - La idea de esta clase es que tome el archivo que esta siendo editado , y recorra linea por linea validandola, segun sea un archivo asm o maq.
-// Si encuentra un error, para el recorrido y muestra un popup diciendo que hubo un error en tal linea, y que error sucedio.
 public class Compilador {
 
     FabricaTraductor fabricaTraductor;
