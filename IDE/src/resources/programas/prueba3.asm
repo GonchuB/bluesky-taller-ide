8795f711ddf0a0e0d5b33ab451156b7022d46ed6
joel <joel@joel-A15>
2014-04-26 01:25:46 -0300

    subo la prueba 3, hay dos lineas comentadas al principio porque si comparo un numero negativo ingresado contra el regirtro r0 que vale 0, salta un null pointer en la instruccion de comparacion. No sè sinceramente por què. si ponen todos numeros positivos anda joya

--- IDE/src/resources/programas/prueba3.asm
+++ IDE/src/resources/programas/prueba3.asm
@@ -1,18 +1,31 @@
-ldm r0,0 ; inicializo r0 en 0
-ldm r1,253 ;Leo de teclado numero de celda inicial y lo pongo en r1
-ldm r2,253 ;Leo un numero
-jpz r2,18 ;Termino de leer numeros
-str r2,r1 ;guardo numero en posicion indicada
-ldm r3,1 ;Steto 1
-;TODO - Setear maximos y minimos a medida q voy leyendo
-add r1,r1,r3 ;Muevo al proximo casillero de memoria
-ldm r2,0 ; seteo r2 en 0
-jpz r2,4 ;Voy a leer otro numero
-ldm r2,253 ;Leo un numero para ver si es maximo o minimo
-jpz r2,30 ;jumpeo si es 0
-ldm r0,1 ;r0 = 1
-jpz r2,34 ;jumpeo es 1
+ldi r0,0
+ldi r1,194 ;guardo en r1 la celda de memoria de incio de datos
+ldi r2,0 ;guardo en r2 el offset a celda de memoria
+ldi r3,1 ;guardo el tamanio de cada instruccion
+ldi r4,0
+cpy r4,r9 ;en r9 guardo el maximo valor ingresado
+ldi r4,127
+cpy r4,r10 ;en r10 guardo el minimo valor ingresado     8
+ldm r4,253 ;obtengo el valor ingresado por teclado
+;cmp r5,r4,r0 ;comparo el valor ingresado con cero
+;jpz r5,16 si el valor ingresado es negativo no lo tengo en cuenta y pido ingresar de nuevo
+jpz r4,46 ;si es cero el valor ingresado
+add r6,r1,r2 ;guardo en r6 la celda donde debo guardar el dato ingresado
+add r2,r2,r3 ;actualizo el offset de celda de datos
+str r4,r6 ;guardo el valor ingresado por teclado en el valor de celda que contiene r6
+cmp r8,r9,r4 ;comparo el valor ingresado contra el maximo
+cmp r11,r4,r10 ;comparo el valor ingresado contra el minimo
+jpz r8,36 ;si el valor ingreaso es mas grande que el maximo lo guardo como maximo 
+jpz r11,42 ;si el valor ingresado es menor que el minimo lo guardo como minimo
+jpz r0,16 ;vuelvo a ingresar un dato nuevo   
+cpy r4,r9 ;en r9 guardo el maximo valor ingresado
+jpz r0,32 ;vuelvo para comparar con el minimo
+jpz r0,16 ;vuelvo a ingresar un dato nuevo  
+cpy r4,r10 ;en r10 guardo el minimo valor ingresado
+jpz r0,16 ;vuelvo a ingresar un dato nuevo               
+ldm r4,253 ;obtengo el valor ingresado por teclado
+jpz r4,54 ;si es cero el valor ingresado muestro el minimo
+stm r9,255 ;muestro por pantalla el maximo
 stp
-stm r4,255 ;Muestro en pantalla el minimo
-stp
-stm r5,255 ;Muestro en pantalla el maximo
+;jpz r0,56 ;salto al final del programa para terminar 
+stm r10,255 ;muestro por pantalla el minimo valor
